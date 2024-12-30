6393ae8983c82948ced13c7fe8516c1bb980c682
Trevor Withers <withers.trevor@gmail.com>
2024-12-29 19:41:11 -0500

    create 52 files Initial commit

---
+++ pantry-log/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Pantry Log',
+  webDir: 'www'
+};
+
+export default config;
